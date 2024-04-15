ea6828f015c143bd38381605b451aa3a76c23cdd
NikKnez <nknezevic07@gmail.com>
2024-04-15 12:15:18 +0200

    Initial commit

---
+++ src/main/java/FilmBox/FilmBoxApplication.java
@@ -0,0 +1,13 @@
+package FilmBox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/FilmBox/FilmBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package FilmBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
